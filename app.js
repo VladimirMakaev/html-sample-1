c3fc9def0f9fa3b2fa84d2651c8c3a8911bdbf65
Vladimir Makaev <vmakaev@gmail.com>
2013-01-14 00:53:42 +0300

    Added blog posts

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.configure('development', function(){
 
 app.get('/', routes.test);
 app.get('/users', user.list);
+app.get('/blog-posts', routes["blog-posts"]);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
